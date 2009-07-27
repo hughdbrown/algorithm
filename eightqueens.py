c38100b4ddf03c0f02ae4443879bda72dc409126
Hugh Brown <hughdbrown@yahoo.com>
2009-07-27 13:45:44 -0400

    Nest helper functions within generator

--- eightqueens.py
+++ eightqueens.py
@@ -2,6 +2,16 @@
 # I found this on the web and I converted it to use python generators.
 
 def queensproblem(solution, rows, columns):
+    def add_one_queen(new_row, columns, solution):
+        for new_column in range(columns):
+            if not conflict(new_row, new_column, solution):
+                yield new_column
+
+    def conflict(new_row, new_column, solution):
+        return any(solution[row]       == new_column or
+                   solution[row] + row == new_column + new_row or
+                   solution[row] - row == new_column - new_row
+                   for row in range(new_row))
     if len(solution) == rows:
         yield solution
     else :
@@ -12,16 +22,6 @@ def queensproblem(solution, rows, columns):
             else:
                 break
  
-def add_one_queen(new_row, columns, solution):
-    for new_column in range(columns):
-        if not conflict(new_row, new_column, solution):
-            yield new_column
-
-def conflict(new_row, new_column, solution):
-    return any(solution[row]       == new_column or
-               solution[row] + row == new_column + new_row or
-               solution[row] - row == new_column - new_row
-               for row in range(new_row))
 
 for i,solution in enumerate(queensproblem([], 8, 8)):
     print i, solution
