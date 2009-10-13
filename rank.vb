736b1e62855844b0c32c55c74d73ad60ce7f0190
Hugh Brown <hughdbrown@yahoo.com>
2009-10-13 15:08:56 -0400

    Uppercase keywords, inline the collection to iterate over

--- rank.vb
+++ rank.vb
@@ -1,13 +1,12 @@
 Function Rank(Of T)(Byref x() as T) as Integer()
 	Dim original_pos = x.Select(Function(xx, index) New With {.Val = xx, .Index = index}).ToLookup(Function(xxx) xxx.Val)
-	dim keys = original_pos.OrderByDescending(Function(yy) yy.Key)
 	Dim result(x.Count-1) as Integer
 	Dim i as Integer = 1
-	for each item in keys
-		for each v in item
+	For Each item in original_pos.OrderByDescending(Function(yy) yy.Key)
+		For Each v in item
 			result(v.Index) = i
-		next
+		Next
 		i = i + item.Count
 	Next
-	return result
+	Return result
 End Function
